c38a6760761289d81cb108aae1c126a0b467ca84
naimasm <113831439+naimasm@users.noreply.github.com>
2024-05-05 10:04:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+    include ('script.php');
+    
+    // fin du script
+    ?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8" />
+     <link rel="stylesheet" href="style.css" />
+
+    <title>Présentation d'OpenClassrooms</title>
+
+</head>
+
+
+<body>
+
+    <h1>Bienvenue sur OpenClassrooms !</h1>
+
+
+    <p CLASS="introduction">Bonjour et bienvenue sur mon site : <em> OpenClassrooms</em> .<br />
+
+       OpenClassrooms, qu'est-ce que c'est ?</p>
+
+
+    <h2>Des cours pour débutants</h2>
+
+    <p> Voici le lien vers ma deuxième page <a href="page2.php"  name="btn_page2"  id="btn_page"> page2</a>
+    </p>
+      <p> Voici le lien vers ma troisième page <a href="page3.html"> page3</a>
+    </p>
+
+    <p><mark>OpenClassrooms</mark> vous propose des cours (tutoriels) destinés aux débutants : aucune connaissance n'est requise pour lire ces cours !</p>
+
+    
+
+    <p>Vous pourrez ainsi apprendre, sans rien y connaître auparavant, à créer un site web, à programmer, à construire des mondes en 3D !</p>
+
+
+    <h2>Une communauté active</h2>
+
+    <a href="https://openclassrooms.com">OpenClassrooms</a>
+
+    <p>Vous avez un problème, un élément du cours que vous ne comprenez pas ? Vous avez besoin d'aide pour créer votre site ?<br />
+
+    Rendez-vous sur les forums ! Vous y découvrirez que vous n'êtes pas le seul dans ce cas et vous trouverez très certainement quelqu'un qui vous aidera aimablement à résoudre votre problème.</p>
+    <Ol>
+
+    <li>Fraises</li>
+     <ul>
+
+    <li>Fraises</li>
+     <ul>
+
+    <li>Fraises</li>
+
+    <li>Framboises</li>
+
+    <li>Cerises</li>
+
+</ul>
+
+    <li>Framboises</li>
+
+    <li>Cerises</li>
+
+</ul>
+
+    <li>Framboises</li>
+
+    <li>Cerises</li>
+
+</Ol>
+
+<p><img src="image/bonneannee.gif" class="imageflottante" alt="Image flottante" /> Ceci est un texte normal de paragraphe, écrit à la suite de l'image et qui l'habillera car l'image est flottante.</p>
+
+<p>Ce texte est écrit à côté de l'image flottante.</p>
+<p class="dessous">Ce texte est écrit sous l'image flottante.</p>
+</body>
+
+</html>
